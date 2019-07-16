d141a5cb8bf4eedbaa028284f1d4f96f5e57fa9e
Yuki-Shibanuma <smatosan@gmail.com>
2019-07-16 20:40:39 +0900

    https

---
+++ middleware/https.js
@@ -0,0 +1,8 @@
+export default ({ req, redirect }) => {
+  if (process.env.buildEnv !== "local" && req && req.url !== "/health") {
+    const protocol = req.headers["x-forwarded-proto"] || (req.connection.encrypted ? "https" : "http")
+    if (protocol === "http") {
+      return redirect(301, `https://${req.headers.host}${req.url}`)
+    }
+  }
+}
