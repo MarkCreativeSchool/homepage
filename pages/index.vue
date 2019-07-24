230e73bf3fdd2bd99039f1abf0acc08b9ec8d531
Yuki-Shibanuma <smatosan@gmail.com>
2019-07-24 12:31:34 +0900

    email

--- pages/index.vue
+++ pages/index.vue
@@ -23,8 +23,7 @@
       </div>
     </div>
     <div class="company">MARK Inc.</div>
-    <div class="tel">029-875-7647</div>
-    <div class="email">markcreativeschool@gmail.com</div>
+    <div class="tel">029-875-7647 / markcreativeschool@gmail.com</div>
     <div class="address">茨城県つくば市花室983-1 嶋村ビル301</div>
   </section>
 </template>
