53352d6041511c0b0471200a0fd5353d649536c3
matoi0317 <matoi.shibanuma@gmail.com>
2024-04-20 16:38:50 +0900

    youtube screen

--- v2.0/components/pages/Parking.vue
+++ v2.0/components/pages/Parking.vue
@@ -25,7 +25,7 @@
         embedCode() {
 
             return `
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${this.videoId}" 
+        <iframe width="50%" height="315" src="https://www.youtube.com/embed/${this.videoId}" 
         title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       `;
         }
