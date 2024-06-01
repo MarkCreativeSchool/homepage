2c183707d05ffddb2af817b5e4358031407c27f3
matoi0317 <matoi.shibanuma@gmail.com>
2024-06-01 10:05:54 +0900

    title change

--- v2.0/nuxt.config.ts
+++ v2.0/nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   devServer: {},
   app: {
     head: {
-      title: "プログラミング・情報技術専門スクール",
+      title: "情報・プログラミング専門スクール MARK - つくば市",
       link: [
         { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@200;400;700&display=swap" },
         { rel: "stylesheet", href: "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" }
